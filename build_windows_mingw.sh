6a8e1f06bea6f0a9e0b240286c2a53b6099b0869
rexim <reximkut@gmail.com>
2023-09-03 04:28:26 +0700

    Build for Windows subsystem in the MinGW build

--- build_windows_mingw.sh
+++ build_windows_mingw.sh
@@ -3,5 +3,5 @@
 set -xe
 
 mkdir -p ./build/
-x86_64-w64-mingw32-gcc -Wall -Wextra -ggdb -Iraylib-4.5.0_win64_mingw-w64/include/  -o ./build/musializer.exe ./src/plug.c ./src/ffmpeg_windows.c ./src/musializer.c -Lraylib-4.5.0_win64_mingw-w64/lib/ -lraylib -lwinmm -lgdi32 -static
+x86_64-w64-mingw32-gcc -mwindows -Wall -Wextra -ggdb -Iraylib-4.5.0_win64_mingw-w64/include/  -o ./build/musializer.exe ./src/plug.c ./src/ffmpeg_windows.c ./src/musializer.c -Lraylib-4.5.0_win64_mingw-w64/lib/ -lraylib -lwinmm -lgdi32 -static
 cp -r ./resources ./build
