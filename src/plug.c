bb4f34f3e6e666ab82b0010f91479ee40c1d71fb
rexim <reximkut@gmail.com>
2023-09-03 00:06:26 +0700

    Make FFT Analyzer part of the plugin state

--- src/plug.c
+++ src/plug.c
@@ -22,6 +22,7 @@
 #define RENDER_HEIGHT (9*RENDER_FACTOR)
 
 typedef struct {
+    // Previewer
     char *file_path;
     Music music;
     Font font;
@@ -30,41 +31,43 @@ typedef struct {
     int circle_power_location;
     bool error;
 
+    // Renderer
     bool rendering;
     RenderTexture2D screen;
     Wave wave;
     float *wave_samples;
     size_t wave_cursor;
     FFMPEG *ffmpeg;
+
+    // FFT Analyzer
+    float in_raw[N];
+    float in_win[N];
+    float complex out_raw[N];
+    float out_log[N];
+    float out_smooth[N];
+    float out_smear[N];
 } Plug;
 
 Plug *plug = NULL;
-
-float in_raw[N];
-float in_win[N];
-float complex out_raw[N];
-float out_log[N];
-float out_smooth[N];
-float out_smear[N];
 
 bool fft_settled(void)
 {
     float eps = 1e-3;
     for (size_t i = 0; i < N; ++i) {
-        if (out_smooth[i] > eps) return false;
-        if (out_smear[i] > eps) return false;
+        if (plug->out_smooth[i] > eps) return false;
+        if (plug->out_smear[i] > eps) return false;
     }
     return true;
 }
 
 void fft_clean(void)
 {
-    memset(in_raw, 0, sizeof(in_raw));
-    memset(in_win, 0, sizeof(in_win));
-    memset(out_raw, 0, sizeof(out_raw));
-    memset(out_log, 0, sizeof(out_log));
-    memset(out_smooth, 0, sizeof(out_smooth));
-    memset(out_smear, 0, sizeof(out_smear));
+    memset(plug->in_raw, 0, sizeof(plug->in_raw));
+    memset(plug->in_win, 0, sizeof(plug->in_win));
+    memset(plug->out_raw, 0, sizeof(plug->out_raw));
+    memset(plug->out_log, 0, sizeof(plug->out_log));
+    memset(plug->out_smooth, 0, sizeof(plug->out_smooth));
+    memset(plug->out_smear, 0, sizeof(plug->out_smear));
 }
 
 // Ported from https://rosettacode.org/wiki/Fast_Fourier_transform#Python
@@ -102,11 +105,11 @@ size_t fft_analyze(float dt)
     for (size_t i = 0; i < N; ++i) {
         float t = (float)i/(N - 1);
         float hann = 0.5 - 0.5*cosf(2*PI*t);
-        in_win[i] = in_raw[i]*hann;
+        plug->in_win[i] = plug->in_raw[i]*hann;
     }
 
     // FFT
-    fft(in_win, 1, out_raw, N);
+    fft(plug->in_win, 1, plug->out_raw, N);
 
     // "Squash" into the Logarithmic Scale
     float step = 1.06;
@@ -117,24 +120,24 @@ size_t fft_analyze(float dt)
         float f1 = ceilf(f*step);
         float a = 0.0f;
         for (size_t q = (size_t) f; q < N/2 && q < (size_t) f1; ++q) {
-            float b = amp(out_raw[q]);
+            float b = amp(plug->out_raw[q]);
             if (b > a) a = b;
         }
         if (max_amp < a) max_amp = a;
-        out_log[m++] = a;
+        plug->out_log[m++] = a;
     }
 
     // Normalize Frequencies to 0..1 range
     for (size_t i = 0; i < m; ++i) {
-        out_log[i] /= max_amp;
+        plug->out_log[i] /= max_amp;
     }
 
     // Smooth out and smear the values
     for (size_t i = 0; i < m; ++i) {
         float smoothness = 8;
-        out_smooth[i] += (out_log[i] - out_smooth[i])*smoothness*dt;
+        plug->out_smooth[i] += (plug->out_log[i] - plug->out_smooth[i])*smoothness*dt;
         float smearness = 3;
-        out_smear[i] += (out_smooth[i] - out_smear[i])*smearness*dt;
+        plug->out_smear[i] += (plug->out_smooth[i] - plug->out_smear[i])*smearness*dt;
     }
 
     return m;
@@ -151,7 +154,7 @@ void fft_render(size_t w, size_t h, size_t m)
 
     // Display the Bars
     for (size_t i = 0; i < m; ++i) {
-        float t = out_smooth[i];
+        float t = plug->out_smooth[i];
         float hue = (float)i/m;
         Color color = ColorFromHSV(hue*360, saturation, value);
         Vector2 startPos = {
@@ -173,8 +176,8 @@ void fft_render(size_t w, size_t h, size_t m)
     SetShaderValue(plug->circle, plug->circle_power_location, (float[1]){ 3.0f }, SHADER_UNIFORM_FLOAT);
     BeginShaderMode(plug->circle);
     for (size_t i = 0; i < m; ++i) {
-        float start = out_smear[i];
-        float end = out_smooth[i];
+        float start = plug->out_smear[i];
+        float end = plug->out_smooth[i];
         float hue = (float)i/m;
         Color color = ColorFromHSV(hue*360, saturation, value);
         Vector2 startPos = {
@@ -214,7 +217,7 @@ void fft_render(size_t w, size_t h, size_t m)
     SetShaderValue(plug->circle, plug->circle_power_location, (float[1]){ 5.0f }, SHADER_UNIFORM_FLOAT);
     BeginShaderMode(plug->circle);
     for (size_t i = 0; i < m; ++i) {
-        float t = out_smooth[i];
+        float t = plug->out_smooth[i];
         float hue = (float)i/m;
         Color color = ColorFromHSV(hue*360, saturation, value);
         Vector2 center = {
@@ -234,8 +237,8 @@ void fft_render(size_t w, size_t h, size_t m)
 
 void fft_push(float frame)
 {
-    memmove(in_raw, in_raw + 1, (N - 1)*sizeof(in_raw[0]));
-    in_raw[N-1] = frame;
+    memmove(plug->in_raw, plug->in_raw + 1, (N - 1)*sizeof(plug->in_raw[0]));
+    plug->in_raw[N-1] = frame;
 }
 
 void callback(void *bufferData, unsigned int frames)
@@ -384,7 +387,7 @@ void plug_update(void)
                 UnloadWaveSamples(plug->wave_samples);
                 plug->rendering = false;
                 fft_clean();
-                PlayMusicStream(plug->music);                
+                PlayMusicStream(plug->music);
             }
 
             const char *label = "Failed to start FFmpeg (press ESC)";
