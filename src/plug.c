00b7ab1be0cd96db0a89abb69aeb804c54b9e454
rexim <reximkut@gmail.com>
2023-09-03 00:09:18 +0700

    Rename global variable plug -> p
    Since it's accessed so often, it probably makes sense to name it
    shorter to reduce the typing and declutter the code a bit.

--- src/plug.c
+++ src/plug.c
@@ -22,7 +22,7 @@
 #define RENDER_HEIGHT (9*RENDER_FACTOR)
 
 typedef struct {
-    // Previewer
+    // Visualizer
     char *file_path;
     Music music;
     Font font;
@@ -48,26 +48,26 @@ typedef struct {
     float out_smear[N];
 } Plug;
 
-Plug *plug = NULL;
+Plug *p = NULL;
 
 bool fft_settled(void)
 {
     float eps = 1e-3;
     for (size_t i = 0; i < N; ++i) {
-        if (plug->out_smooth[i] > eps) return false;
-        if (plug->out_smear[i] > eps) return false;
+        if (p->out_smooth[i] > eps) return false;
+        if (p->out_smear[i] > eps) return false;
     }
     return true;
 }
 
 void fft_clean(void)
 {
-    memset(plug->in_raw, 0, sizeof(plug->in_raw));
-    memset(plug->in_win, 0, sizeof(plug->in_win));
-    memset(plug->out_raw, 0, sizeof(plug->out_raw));
-    memset(plug->out_log, 0, sizeof(plug->out_log));
-    memset(plug->out_smooth, 0, sizeof(plug->out_smooth));
-    memset(plug->out_smear, 0, sizeof(plug->out_smear));
+    memset(p->in_raw, 0, sizeof(p->in_raw));
+    memset(p->in_win, 0, sizeof(p->in_win));
+    memset(p->out_raw, 0, sizeof(p->out_raw));
+    memset(p->out_log, 0, sizeof(p->out_log));
+    memset(p->out_smooth, 0, sizeof(p->out_smooth));
+    memset(p->out_smear, 0, sizeof(p->out_smear));
 }
 
 // Ported from https://rosettacode.org/wiki/Fast_Fourier_transform#Python
@@ -105,11 +105,11 @@ size_t fft_analyze(float dt)
     for (size_t i = 0; i < N; ++i) {
         float t = (float)i/(N - 1);
         float hann = 0.5 - 0.5*cosf(2*PI*t);
-        plug->in_win[i] = plug->in_raw[i]*hann;
+        p->in_win[i] = p->in_raw[i]*hann;
     }
 
     // FFT
-    fft(plug->in_win, 1, plug->out_raw, N);
+    fft(p->in_win, 1, p->out_raw, N);
 
     // "Squash" into the Logarithmic Scale
     float step = 1.06;
@@ -120,24 +120,24 @@ size_t fft_analyze(float dt)
         float f1 = ceilf(f*step);
         float a = 0.0f;
         for (size_t q = (size_t) f; q < N/2 && q < (size_t) f1; ++q) {
-            float b = amp(plug->out_raw[q]);
+            float b = amp(p->out_raw[q]);
             if (b > a) a = b;
         }
         if (max_amp < a) max_amp = a;
-        plug->out_log[m++] = a;
+        p->out_log[m++] = a;
     }
 
     // Normalize Frequencies to 0..1 range
     for (size_t i = 0; i < m; ++i) {
-        plug->out_log[i] /= max_amp;
+        p->out_log[i] /= max_amp;
     }
 
     // Smooth out and smear the values
     for (size_t i = 0; i < m; ++i) {
         float smoothness = 8;
-        plug->out_smooth[i] += (plug->out_log[i] - plug->out_smooth[i])*smoothness*dt;
+        p->out_smooth[i] += (p->out_log[i] - p->out_smooth[i])*smoothness*dt;
         float smearness = 3;
-        plug->out_smear[i] += (plug->out_smooth[i] - plug->out_smear[i])*smearness*dt;
+        p->out_smear[i] += (p->out_smooth[i] - p->out_smear[i])*smearness*dt;
     }
 
     return m;
@@ -154,7 +154,7 @@ void fft_render(size_t w, size_t h, size_t m)
 
     // Display the Bars
     for (size_t i = 0; i < m; ++i) {
-        float t = plug->out_smooth[i];
+        float t = p->out_smooth[i];
         float hue = (float)i/m;
         Color color = ColorFromHSV(hue*360, saturation, value);
         Vector2 startPos = {
@@ -172,12 +172,12 @@ void fft_render(size_t w, size_t h, size_t m)
     Texture2D texture = { rlGetTextureIdDefault(), 1, 1, 1, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8 };
 
     // Display the Smears
-    SetShaderValue(plug->circle, plug->circle_radius_location, (float[1]){ 0.3f }, SHADER_UNIFORM_FLOAT);
-    SetShaderValue(plug->circle, plug->circle_power_location, (float[1]){ 3.0f }, SHADER_UNIFORM_FLOAT);
-    BeginShaderMode(plug->circle);
+    SetShaderValue(p->circle, p->circle_radius_location, (float[1]){ 0.3f }, SHADER_UNIFORM_FLOAT);
+    SetShaderValue(p->circle, p->circle_power_location, (float[1]){ 3.0f }, SHADER_UNIFORM_FLOAT);
+    BeginShaderMode(p->circle);
     for (size_t i = 0; i < m; ++i) {
-        float start = plug->out_smear[i];
-        float end = plug->out_smooth[i];
+        float start = p->out_smear[i];
+        float end = p->out_smooth[i];
         float hue = (float)i/m;
         Color color = ColorFromHSV(hue*360, saturation, value);
         Vector2 startPos = {
@@ -213,11 +213,11 @@ void fft_render(size_t w, size_t h, size_t m)
     EndShaderMode();
 
     // Display the Circles
-    SetShaderValue(plug->circle, plug->circle_radius_location, (float[1]){ 0.07f }, SHADER_UNIFORM_FLOAT);
-    SetShaderValue(plug->circle, plug->circle_power_location, (float[1]){ 5.0f }, SHADER_UNIFORM_FLOAT);
-    BeginShaderMode(plug->circle);
+    SetShaderValue(p->circle, p->circle_radius_location, (float[1]){ 0.07f }, SHADER_UNIFORM_FLOAT);
+    SetShaderValue(p->circle, p->circle_power_location, (float[1]){ 5.0f }, SHADER_UNIFORM_FLOAT);
+    BeginShaderMode(p->circle);
     for (size_t i = 0; i < m; ++i) {
-        float t = plug->out_smooth[i];
+        float t = p->out_smooth[i];
         float hue = (float)i/m;
         Color color = ColorFromHSV(hue*360, saturation, value);
         Vector2 center = {
@@ -237,8 +237,8 @@ void fft_render(size_t w, size_t h, size_t m)
 
 void fft_push(float frame)
 {
-    memmove(plug->in_raw, plug->in_raw + 1, (N - 1)*sizeof(plug->in_raw[0]));
-    plug->in_raw[N-1] = frame;
+    memmove(p->in_raw, p->in_raw + 1, (N - 1)*sizeof(p->in_raw[0]));
+    p->in_raw[N-1] = frame;
 }
 
 void callback(void *bufferData, unsigned int frames)
@@ -253,41 +253,41 @@ void callback(void *bufferData, unsigned int frames)
 
 void plug_init(void)
 {
-    plug = malloc(sizeof(*plug));
-    assert(plug != NULL && "Buy more RAM lol");
-    memset(plug, 0, sizeof(*plug));
+    p = malloc(sizeof(*p));
+    assert(p != NULL && "Buy more RAM lol");
+    memset(p, 0, sizeof(*p));
 
-    plug->font = LoadFontEx("./resources/fonts/Alegreya-Regular.ttf", FONT_SIZE, NULL, 0);
+    p->font = LoadFontEx("./resources/fonts/Alegreya-Regular.ttf", FONT_SIZE, NULL, 0);
     // TODO: Maybe we should try to keep compiling different versions of shaders
     // until one of them works?
     //
     // If the shader can not be compiled maybe we could fallback to software rendering
     // of the texture of a fuzzy circle? The shader does not really do anything particularly
     // special.
-    plug->circle = LoadShader(NULL, TextFormat("./resources/shaders/glsl%d/circle.fs", GLSL_VERSION));
-    plug->circle_radius_location = GetShaderLocation(plug->circle, "radius");
-    plug->circle_power_location = GetShaderLocation(plug->circle, "power");
-    plug->screen = LoadRenderTexture(RENDER_WIDTH, RENDER_HEIGHT);
+    p->circle = LoadShader(NULL, TextFormat("./resources/shaders/glsl%d/circle.fs", GLSL_VERSION));
+    p->circle_radius_location = GetShaderLocation(p->circle, "radius");
+    p->circle_power_location = GetShaderLocation(p->circle, "power");
+    p->screen = LoadRenderTexture(RENDER_WIDTH, RENDER_HEIGHT);
 }
 
 Plug *plug_pre_reload(void)
 {
-    if (IsMusicReady(plug->music)) {
-        DetachAudioStreamProcessor(plug->music.stream, callback);
+    if (IsMusicReady(p->music)) {
+        DetachAudioStreamProcessor(p->music.stream, callback);
     }
-    return plug;
+    return p;
 }
 
-void plug_post_reload(Plug *prev)
+void plug_post_reload(Plug *pp)
 {
-    plug = prev;
-    if (IsMusicReady(plug->music)) {
-        AttachAudioStreamProcessor(plug->music.stream, callback);
+    p = pp;
+    if (IsMusicReady(p->music)) {
+        AttachAudioStreamProcessor(p->music.stream, callback);
     }
-    UnloadShader(plug->circle);
-    plug->circle = LoadShader(NULL, TextFormat("./resources/shaders/glsl%d/circle.fs", GLSL_VERSION));
-    plug->circle_radius_location = GetShaderLocation(plug->circle, "radius");
-    plug->circle_power_location = GetShaderLocation(plug->circle, "power");
+    UnloadShader(p->circle);
+    p->circle = LoadShader(NULL, TextFormat("./resources/shaders/glsl%d/circle.fs", GLSL_VERSION));
+    p->circle_radius_location = GetShaderLocation(p->circle, "radius");
+    p->circle_power_location = GetShaderLocation(p->circle, "power");
 }
 
 void plug_update(void)
@@ -298,27 +298,27 @@ void plug_update(void)
     if (IsFileDropped()) {
         FilePathList droppedFiles = LoadDroppedFiles();
         if (droppedFiles.count > 0) {
-            free(plug->file_path);
-            plug->file_path = strdup(droppedFiles.paths[0]);
+            free(p->file_path);
+            p->file_path = strdup(droppedFiles.paths[0]);
 
-            if (IsMusicReady(plug->music)) {
-                StopMusicStream(plug->music);
-                UnloadMusicStream(plug->music);
+            if (IsMusicReady(p->music)) {
+                StopMusicStream(p->music);
+                UnloadMusicStream(p->music);
             }
 
-            plug->music = LoadMusicStream(plug->file_path);
+            p->music = LoadMusicStream(p->file_path);
 
-            if (IsMusicReady(plug->music)) {
-                plug->error = false;
-                printf("music.frameCount = %u\n", plug->music.frameCount);
-                printf("music.stream.sampleRate = %u\n", plug->music.stream.sampleRate);
-                printf("music.stream.sampleSize = %u\n", plug->music.stream.sampleSize);
-                printf("music.stream.channels = %u\n", plug->music.stream.channels);
-                SetMusicVolume(plug->music, 0.5f);
-                AttachAudioStreamProcessor(plug->music.stream, callback);
-                PlayMusicStream(plug->music);
+            if (IsMusicReady(p->music)) {
+                p->error = false;
+                printf("music.frameCount = %u\n", p->music.frameCount);
+                printf("music.stream.sampleRate = %u\n", p->music.stream.sampleRate);
+                printf("music.stream.sampleSize = %u\n", p->music.stream.sampleSize);
+                printf("music.stream.channels = %u\n", p->music.stream.channels);
+                SetMusicVolume(p->music, 0.5f);
+                AttachAudioStreamProcessor(p->music.stream, callback);
+                PlayMusicStream(p->music);
             } else {
-                plug->error = true;
+                p->error = true;
             }
         }
         UnloadDroppedFiles(droppedFiles);
@@ -329,34 +329,34 @@ void plug_update(void)
         0x15, 0x15, 0x15, 0xFF
     });
 
-    if (!plug->rendering) {
-        if (IsMusicReady(plug->music)) {
-            UpdateMusicStream(plug->music);
+    if (!p->rendering) {
+        if (IsMusicReady(p->music)) {
+            UpdateMusicStream(p->music);
 
             if (IsKeyPressed(KEY_SPACE)) {
-                if (IsMusicStreamPlaying(plug->music)) {
-                    PauseMusicStream(plug->music);
+                if (IsMusicStreamPlaying(p->music)) {
+                    PauseMusicStream(p->music);
                 } else {
-                    ResumeMusicStream(plug->music);
+                    ResumeMusicStream(p->music);
                 }
             }
 
             if (IsKeyPressed(KEY_Q)) {
-                StopMusicStream(plug->music);
-                PlayMusicStream(plug->music);
+                StopMusicStream(p->music);
+                PlayMusicStream(p->music);
             }
 
             if (IsKeyPressed(KEY_F)) {
-                StopMusicStream(plug->music);
+                StopMusicStream(p->music);
 
                 fft_clean();
-                plug->wave = LoadWave(plug->file_path);
-                plug->wave_cursor = 0;
-                plug->wave_samples = LoadWaveSamples(plug->wave);
+                p->wave = LoadWave(p->file_path);
+                p->wave_cursor = 0;
+                p->wave_samples = LoadWaveSamples(p->wave);
                 // TODO: set the rendering output path based on the input path
                 // Basically output into the same folder
-                plug->ffmpeg = ffmpeg_start_rendering(plug->screen.texture.width, plug->screen.texture.height, RENDER_FPS, plug->file_path);
-                plug->rendering = true;
+                p->ffmpeg = ffmpeg_start_rendering(p->screen.texture.width, p->screen.texture.height, RENDER_FPS, p->file_path);
+                p->rendering = true;
                 SetTraceLogLevel(LOG_NONE);
             }
 
@@ -365,81 +365,81 @@ void plug_update(void)
         } else {
             const char *label;
             Color color;
-            if (plug->error) {
+            if (p->error) {
                 label = "Could not load file";
                 color = RED;
             } else {
                 label = "Drag&Drop Music Here";
                 color = WHITE;
             }
-            Vector2 size = MeasureTextEx(plug->font, label, plug->font.baseSize, 0);
+            Vector2 size = MeasureTextEx(p->font, label, p->font.baseSize, 0);
             Vector2 position = {
                 w/2 - size.x/2,
                 h/2 - size.y/2,
             };
-            DrawTextEx(plug->font, label, position, plug->font.baseSize, 0, color);
+            DrawTextEx(p->font, label, position, p->font.baseSize, 0, color);
         }
     } else {
-        if (plug->ffmpeg == NULL) {
+        if (p->ffmpeg == NULL) {
             if (IsKeyPressed(KEY_ESCAPE)) {
                 SetTraceLogLevel(LOG_INFO);
-                UnloadWave(plug->wave);
-                UnloadWaveSamples(plug->wave_samples);
-                plug->rendering = false;
+                UnloadWave(p->wave);
+                UnloadWaveSamples(p->wave_samples);
+                p->rendering = false;
                 fft_clean();
-                PlayMusicStream(plug->music);
+                PlayMusicStream(p->music);
             }
 
             const char *label = "Failed to start FFmpeg (press ESC)";
             Color color = RED;
-            Vector2 size = MeasureTextEx(plug->font, label, plug->font.baseSize, 0);
+            Vector2 size = MeasureTextEx(p->font, label, p->font.baseSize, 0);
             Vector2 position = {
                 w/2 - size.x/2,
                 h/2 - size.y/2,
             };
-            DrawTextEx(plug->font, label, position, plug->font.baseSize, 0, color);
+            DrawTextEx(p->font, label, position, p->font.baseSize, 0, color);
         } else {
-            if (plug->wave_cursor >= plug->wave.frameCount && fft_settled()) {
-                ffmpeg_end_rendering(plug->ffmpeg);
+            if (p->wave_cursor >= p->wave.frameCount && fft_settled()) {
+                ffmpeg_end_rendering(p->ffmpeg);
                 SetTraceLogLevel(LOG_INFO);
-                UnloadWave(plug->wave);
-                UnloadWaveSamples(plug->wave_samples);
-                plug->rendering = false;
+                UnloadWave(p->wave);
+                UnloadWaveSamples(p->wave_samples);
+                p->rendering = false;
                 fft_clean();
-                PlayMusicStream(plug->music);
+                PlayMusicStream(p->music);
             } else {
                 // TODO: indicate the rendering progress
                 const char *label = "Rendering video...";
                 Color color = WHITE;
 
-                Vector2 size = MeasureTextEx(plug->font, label, plug->font.baseSize, 0);
+                Vector2 size = MeasureTextEx(p->font, label, p->font.baseSize, 0);
                 Vector2 position = {
                     w/2 - size.x/2,
                     h/2 - size.y/2,
                 };
-                DrawTextEx(plug->font, label, position, plug->font.baseSize, 0, color);
+                DrawTextEx(p->font, label, position, p->font.baseSize, 0, color);
 
-                size_t chunk_size = plug->wave.sampleRate/RENDER_FPS;
+                size_t chunk_size = p->wave.sampleRate/RENDER_FPS;
                 // https://cdecl.org/?q=float+%28*fs%29%5B2%5D
-                float (*fs)[plug->wave.channels] = (void*)plug->wave_samples;
+                float (*fs)[p->wave.channels] = (void*)p->wave_samples;
                 for (size_t i = 0; i < chunk_size; ++i) {
-                    if (plug->wave_cursor < plug->wave.frameCount) {
-                        fft_push(fs[plug->wave_cursor][0]);
+                    if (p->wave_cursor < p->wave.frameCount) {
+                        fft_push(fs[p->wave_cursor][0]);
                     } else {
                         fft_push(0);
                     }
-                    plug->wave_cursor += 1;
+                    p->wave_cursor += 1;
                 }
 
                 size_t m = fft_analyze(1.0f/RENDER_FPS);
 
-                BeginTextureMode(plug->screen);
+                BeginTextureMode(p->screen);
                 ClearBackground(CLITERAL(Color) {0x15, 0x15, 0x15, 0xFF});
-                fft_render(plug->screen.texture.width, plug->screen.texture.height, m);
+                fft_render(p->screen.texture.width, p->screen.texture.height, m);
                 EndTextureMode();
 
-                Image image = LoadImageFromTexture(plug->screen.texture);
-                ffmpeg_send_frame_flipped(plug->ffmpeg, image.data, image.width, image.height);
+                Image image = LoadImageFromTexture(p->screen.texture);
+                ffmpeg_send_frame_flipped(p->ffmpeg, image.data, image.width, image.height);
                 UnloadImage(image);
             }
         }
