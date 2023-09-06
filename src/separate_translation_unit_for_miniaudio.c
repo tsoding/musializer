93e47fc5c79b44521bf4872200ad5917a9d7c049
rexim <reximkut@gmail.com>
2023-09-06 18:53:09 +0700

    Factor out raylib_log.h

--- src/separate_translation_unit_for_miniaudio.c
+++ src/separate_translation_unit_for_miniaudio.c
@@ -1,8 +1,11 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "miniaudio.h"
+
 #include "separate_translation_unit_for_miniaudio.h"
+
+#include "miniaudio.h"
+#include "raylib_log.h"
 
 ma_encoder_config encoderConfig;
 
@@ -27,7 +30,7 @@ void *init_audio_devices(user_callback2_t *user_callback)
     assert(device != NULL && "Buy MORE RAM lol!!");
     ma_result result = ma_device_init(NULL, &deviceConfig, device);
     if (result != MA_SUCCESS) {
-        fprintf(stderr, "Failed to initialize capture device.\n");
+        TraceLog(LOG_ERROR, "MINIAUDIO: Failed to initialize capture device: %s", ma_result_description(result));
         return NULL;
     }
 
@@ -39,7 +42,7 @@ bool start_the_device(void *device)
     ma_result result = ma_device_start(device);
     if (result != MA_SUCCESS) {
         ma_device_uninit(device);
-        printf("Failed to start device.\n");
+        TraceLog(LOG_ERROR, "MINIAUDIO: Failed to start device: %s", ma_result_description(result));
         return false;
     }
 
--- src/separate_translation_unit_for_miniaudio.h
+++ src/separate_translation_unit_for_miniaudio.h
@@ -1,3 +1,8 @@
+// Since miniaudio.h includes windows.h on Windows this separate translation
+// unit was specifically created to avoid collisions between symbols from windows.h
+// and raylib.h. The interface of this unit should not depend on anything
+// from miniaudio.h.
+
 #ifndef SEPARATE_TRANSLATION_UNIT_FOR_MINIAUDIO_H_
 #define SEPARATE_TRANSLATION_UNIT_FOR_MINIAUDIO_H_
 
