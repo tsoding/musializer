52dddeec323df7dca6343cd78f7e4dcaaaeeef2f
rexim <reximkut@gmail.com>
2023-09-18 21:55:17 +0700

    Remove rebuild_logo.sh

--- rebuild_logo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-convert -background None ./resources/logo/logo.svg -resize 256 ./resources/logo/logo-256.ico
-convert -background None ./resources/logo/logo.svg -resize 256 ./resources/logo/logo-256.png
