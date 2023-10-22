b4e6128679ac483708cfff27e9411c1707a05cb3
OetkenPurveyorOfCode <alexander.x.oetken@gmail.com>
2023-10-22 18:19:44 +0200

    Fix for issue #29: Change the Codepage of the process to UTF-8
    To support loading filenames with unicode characters on windows, declare UTF-8
    support in the manifest file.

--- nob.c
+++ nob.c
@@ -327,6 +327,12 @@ bool build_musializer(Config config)
                 nob_return_defer(false);
             } else {
                 cmd.count = 0;
+                    nob_cmd_append(&cmd, "rc");
+                    nob_cmd_append(&cmd, "/fo", "./build/musializer.res");
+                    nob_cmd_append(&cmd, "./src/musializer.rc");
+                    // NOTE: Do not change the order of commandline arguments to rc. Their argparser is weird.
+                if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
+                cmd.count = 0;
                     nob_cmd_append(&cmd, "cl.exe");
                     if (config.microphone) nob_cmd_append(&cmd, "/DFEATURE_MICROPHONE");
                     nob_cmd_append(&cmd, "/I", "./raylib/raylib-4.5.0/src/");
@@ -336,13 +342,12 @@ bool build_musializer(Config config)
                         "./src/plug.c",
                         "./src/ffmpeg_windows.c"
                         // TODO: building resource file is not implemented for TARGET_WIN64_MSVC
-                        //"./build/musializer.res"
                         );
                     nob_cmd_append(&cmd,
                         "/link",
                         nob_temp_sprintf("/LIBPATH:build/raylib/%s", NOB_ARRAY_GET(target_names, config.target)),
                         "raylib.lib");
-                    nob_cmd_append(&cmd, "Winmm.lib", "gdi32.lib", "User32.lib", "Shell32.lib");
+                    nob_cmd_append(&cmd, "Winmm.lib", "gdi32.lib", "User32.lib", "Shell32.lib", "./build/musializer.res");
                     // TODO: is some sort of `-static` flag needed for MSVC to get a statically linked executable
                     //nob_cmd_append(&cmd, "-static");
                 if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
