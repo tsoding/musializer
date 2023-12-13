a3bf67360da3676d4d29432217d35d7ed3e52cda
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-12-13 04:39:51 +0100

    Fix Mac hot reload

--- src/hotreload_mac.c
+++ src/hotreload_mac.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dlfcn.h>
+#include <mach-o/dyld.h>
+#include <limits.h>
+#include <string.h>
 
 #include <raylib.h>
 
@@ -16,7 +19,30 @@ bool reload_libplug(void)
 {
     if (libplug != NULL) dlclose(libplug);
 
-    libplug = dlopen(libplug_file_name, RTLD_NOW);
+    // I'm not sure if this is the best way to do this but Basically
+    // on MacOS the library lookup is a bit weird.
+    //
+    // The easiest way to load the library is to use the full path,
+    // so we get the executable path and replace the executable name
+    // with the library name.
+    char executable_path[PATH_MAX];
+    uint32_t bufsize = PATH_MAX;
+    if(_NSGetExecutablePath(executable_path, &bufsize)) {
+        TraceLog(LOG_ERROR, "HOTRELOAD: could not get executable path");
+        return false;
+    }
+    // Replace the binary name with the library name.
+    //
+    // The easiest way to do this is to find the last slash and
+    // replace everything after it with the library name.
+    char *last_slash = strrchr(executable_path, '/');
+    if (last_slash == NULL) {
+        TraceLog(LOG_ERROR, "HOTRELOAD: could not find executable path");
+        return false;
+    }
+    strcpy(last_slash + 1, libplug_file_name);
+
+    libplug = dlopen(executable_path, RTLD_NOW);
     if (libplug == NULL) {
         TraceLog(LOG_ERROR, "HOTRELOAD: could not load %s: %s", libplug_file_name, dlerror());
         return false;
