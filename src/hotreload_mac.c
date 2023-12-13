7f4675d925309b37e50cc3ec35fbea44c2424983
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-12-13 03:22:32 +0100

    Add hotreload_mac source file

---
+++ src/hotreload_mac.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <dlfcn.h>
+
+#include <raylib.h>
+
+#include "hotreload.h"
+
+static const char *libplug_file_name = "libplug.dylib";
+static void *libplug = NULL;
+
+#define PLUG(name, ...) name##_t *name = NULL;
+LIST_OF_PLUGS
+#undef PLUG
+
+bool reload_libplug(void)
+{
+    if (libplug != NULL) dlclose(libplug);
+
+    libplug = dlopen(libplug_file_name, RTLD_NOW);
+    if (libplug == NULL) {
+        TraceLog(LOG_ERROR, "HOTRELOAD: could not load %s: %s", libplug_file_name, dlerror());
+        return false;
+    }
+
+    #define PLUG(name, ...) \
+        name = dlsym(libplug, #name); \
+        if (name == NULL) { \
+            TraceLog(LOG_ERROR, "HOTRELOAD: could not find %s symbol in %s: %s", \
+                     #name, libplug_file_name, dlerror()); \
+            return false; \
+        }
+    LIST_OF_PLUGS
+    #undef PLUG
+
+    return true;
+}
