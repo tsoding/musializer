88703e1ebc1be47dfa53173f2af0a5b94c674593
MarkosTh09 <markos.theocharis2009@gmail.com>
2024-02-26 21:21:37 +0400

    add hotreload support for windows

--- src/nob_win64_msvc.c
+++ src/nob_win64_msvc.c
@@ -7,16 +7,49 @@ bool build_musializer(void)
     Nob_Cmd cmd = {0};
     Nob_Procs procs = {0};
 
-#ifdef MUSIALIZER_HOTRELOAD
-    nob_log(NOB_ERROR, "TODO: hotreloading is not supported on %s yet", NOB_ARRAY_GET(target_names, config.target));
-    nob_return_defer(false);
-#else
     cmd.count = 0;
         nob_cmd_append(&cmd, "rc");
         nob_cmd_append(&cmd, "/fo", "./build/musializer.res");
         nob_cmd_append(&cmd, "./src/musializer.rc");
         // NOTE: Do not change the order of commandline arguments to rc. Their argparser is weird.
     if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
+#ifdef MUSIALIZER_HOTRELOAD
+    procs.count = 0;
+        cmd.count = 0;
+            nob_cmd_append(&cmd, "cl.exe");
+            nob_cmd_append(&cmd, "/LD");
+            nob_cmd_append(&cmd, "/Fobuild\\", "/Fe./build/libplug.dll");
+            nob_cmd_append(&cmd, "/I", "./build/");
+            nob_cmd_append(&cmd, "/I", "./raylib/raylib-"RAYLIB_VERSION"/src/");
+            nob_cmd_append(&cmd,
+                "src/plug.c",
+                "src/ffmpeg_windows.c");
+            nob_cmd_append(&cmd,
+                "/link",
+                nob_temp_sprintf("/LIBPATH:build/raylib/%s", MUSIALIZER_TARGET_NAME),
+                "raylib.lib");
+            nob_cmd_append(&cmd, "Winmm.lib", "gdi32.lib", "User32.lib", "Shell32.lib");
+        nob_da_append(&procs, nob_cmd_run_async(cmd));
+            
+        cmd.count = 0;
+            nob_cmd_append(&cmd, "cl.exe");
+            nob_cmd_append(&cmd, "/I", "./build/");
+            nob_cmd_append(&cmd, "/I", "./raylib/raylib-"RAYLIB_VERSION"/src/");
+            nob_cmd_append(&cmd, "/Fobuild\\", "/Febuild\\musializer.exe");
+            nob_cmd_append(&cmd,
+                "./src/musializer.c",
+                "./src/hotreload_windows.c",
+                );
+            nob_cmd_append(&cmd,
+                "/link",
+                "/SUBSYSTEM:WINDOWS",
+                "/entry:mainCRTStartup",
+                nob_temp_sprintf("/LIBPATH:build/raylib/%s", MUSIALIZER_TARGET_NAME),
+                "raylib.lib");
+            nob_cmd_append(&cmd, "Winmm.lib", "gdi32.lib", "User32.lib", "Shell32.lib", "./build/musializer.res");
+        nob_da_append(&procs, nob_cmd_run_async(cmd));
+    if (!nob_procs_wait(procs)) nob_return_defer(false);
+#else
     cmd.count = 0;
         nob_cmd_append(&cmd, "cl.exe");
         nob_cmd_append(&cmd, "/I", "./build/");
@@ -25,9 +58,7 @@ bool build_musializer(void)
         nob_cmd_append(&cmd,
             "./src/musializer.c",
             "./src/plug.c",
-            "./src/ffmpeg_windows.c"
-            // TODO: building resource file is not implemented for TARGET_WIN64_MSVC
-            );
+            "./src/ffmpeg_windows.c");
         nob_cmd_append(&cmd,
             "/link",
             "/SUBSYSTEM:WINDOWS",
@@ -73,6 +104,9 @@ bool build_raylib(void)
         if (nob_needs_rebuild(output_path, &input_path, 1)) {
             cmd.count = 0;
             nob_cmd_append(&cmd, "cl.exe", "/DPLATFORM_DESKTOP", "/DSUPPORT_FILEFORMAT_FLAC=1");
+            #ifdef MUSIALIZER_HOTRELOAD
+                nob_cmd_append(&cmd, "/DBUILD_LIBTYPE_SHARED");
+            #endif
             nob_cmd_append(&cmd, "/I", "./raylib/raylib-"RAYLIB_VERSION"/src/external/glfw/include");
             nob_cmd_append(&cmd, "/c", input_path);
             nob_cmd_append(&cmd, nob_temp_sprintf("/Fo%s", output_path));
@@ -83,7 +117,6 @@ bool build_raylib(void)
     cmd.count = 0;
 
     if (!nob_procs_wait(procs)) nob_return_defer(false);
-
 #ifndef MUSIALIZER_HOTRELOAD
     const char *libraylib_path = nob_temp_sprintf("%s/raylib.lib", build_path);
     if (nob_needs_rebuild(libraylib_path, object_files.items, object_files.count)) {
@@ -96,10 +129,19 @@ bool build_raylib(void)
         if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
     }
 #else
-    nob_log(NOB_WARNING, "TODO: dynamic raylib for %s is not supported yet", NOB_ARRAY_GET(target_names, config.target));
-    nob_return_defer(false);
+    if (nob_needs_rebuild("./build/raylib.dll", object_files.items, object_files.count)) {
+        nob_cmd_append(&cmd, "link.exe", "/DLL");
+        for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i) {
+            const char *input_path = nob_temp_sprintf("%s/%s.obj", build_path, raylib_modules[i]);
+            nob_cmd_append(&cmd, input_path);
+        }
+        nob_cmd_append(&cmd, "Winmm.lib", "gdi32.lib", "User32.lib", "Shell32.lib");
+        nob_cmd_append(&cmd, nob_temp_sprintf("/IMPLIB:%s/raylib.lib", build_path));
+        nob_cmd_append(&cmd, "/OUT:./build/raylib.dll");
+        if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
+    }
 #endif // MUSIALIZER_HOTRELOAD
-
+    
 defer:
     nob_cmd_free(cmd);
     nob_da_free(object_files);
--- src/plug.c
+++ src/plug.c
@@ -110,6 +110,12 @@ void *plug_load_resource(const char *file_path, size_t *size)
 #    define subcc(a, b) ((a)-(b))
 #endif
 
+#ifdef _WIN32
+#define MUSIALIZER_PLUG __declspec(dllexport)
+#else
+#define MUSIALIZER_PLUG
+#endif
+
 typedef struct {
     char *file_path;
     Music music;
@@ -1726,7 +1732,7 @@ static void rendering_screen(void)
     }
 }
 
-void plug_init(void)
+MUSIALIZER_PLUG void plug_init(void)
 {
     p = malloc(sizeof(*p));
     assert(p != NULL && "Buy more RAM lol");
@@ -1762,7 +1768,7 @@ void plug_init(void)
     SetMasterVolume(0.5);
 }
 
-Plug *plug_pre_reload(void)
+MUSIALIZER_PLUG Plug *plug_pre_reload(void)
 {
     for (size_t i = 0; i < p->tracks.count; ++i) {
         Track *it = &p->tracks.items[i];
@@ -1772,7 +1778,7 @@ Plug *plug_pre_reload(void)
     return p;
 }
 
-void plug_post_reload(Plug *pp)
+MUSIALIZER_PLUG void plug_post_reload(Plug *pp)
 {
     p = pp;
     for (size_t i = 0; i < p->tracks.count; ++i) {
@@ -1789,7 +1795,7 @@ void plug_post_reload(Plug *pp)
     p->circle_power_location = GetShaderLocation(p->circle, "power");
 }
 
-void plug_update(void)
+MUSIALIZER_PLUG void plug_update(void)
 {
     BeginDrawing();
     ClearBackground(COLOR_BACKGROUND);
