18ae3d4dbd5f874eb5b46a9615ec733d14dad1e9
rexim <reximkut@gmail.com>
2024-06-10 12:29:29 +0700

    Factor out src_build/configurer.c

--- nob.c
+++ nob.c
@@ -8,64 +8,7 @@
 
 #define NOB_IMPLEMENTATION
 #include "./nob.h"
-
-#define CONFIG_PATH "./build/config.h"
-
-void generate_default_config(Nob_String_Builder *content)
-{
-    nob_sb_append_cstr(content, "//// Build target. Pick only one!\n");
-#ifdef _WIN32
-#   if defined(_MSC_VER)
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_LINUX\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_WIN64_MINGW\n");
-    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET_WIN64_MSVC\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_MACOS\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_OPENBSD\n");
-#   else
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_LINUX\n");
-    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET_WIN64_MINGW\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_WIN64_MSVC\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_MACOS\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_OPENBSD\n");
-#   endif
-#elif defined (__APPLE__) || defined (__MACH__)
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_LINUX\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_WIN64_MINGW\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_WIN64_MSVC\n");
-    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET_MACOS\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_OPENBSD\n");
-#elif defined(__OpenBSD__)
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_LINUX\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_WIN64_MINGW\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_WIN64_MSVC\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_MACOS\n");
-    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET_OPENBSD\n");
-#else
-    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET_LINUX\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_WIN64_MINGW\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_WIN64_MSVC\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_MACOS\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_TARGET_OPENBSD\n");
-#endif
-
-    nob_sb_append_cstr(content, "\n");
-    nob_sb_append_cstr(content, "//// Moves everything in src/plug.c to a separate \"DLL\" so it can be hotreloaded. Works only for Linux right now\n");
-    // TODO: FIX ASAP! This requires bootstrapping nob with additional flags which goes against its philosophy!
-#ifdef MUSIALIZER_HOTRELOAD
-    nob_sb_append_cstr(content, "#define MUSIALIZER_HOTRELOAD\n");
-#else
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_HOTRELOAD\n");
-#endif
-    nob_sb_append_cstr(content, "\n");
-    nob_sb_append_cstr(content, "//// Don't bundle resources/ folder with the executable and load the resources directly from the folder.\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_UNBUNDLE\n");
-    nob_sb_append_cstr(content, "\n");
-    nob_sb_append_cstr(content, "//// Unfinished feature that enables capturing sound from the mic.\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_MICROPHONE\n");
-    nob_sb_append_cstr(content, "\n");
-    nob_sb_append_cstr(content, "//// Activate UI buttons on Press instead of Release just as John Carmack explained https://twitter.com/ID_AA_Carmack/status/1787850053912064005\n");
-    nob_sb_append_cstr(content, "// #define MUSIALIZER_ACT_ON_PRESS\n");
-}
+#include "./src_build/configurer.c"
 
 int main(int argc, char **argv)
 {
@@ -87,16 +30,21 @@ int main(int argc, char **argv)
 
     if (!nob_mkdir_if_not_exists("build")) return 1;
 
+    Nob_String_Builder content = {0};
     int config_exists = nob_file_exists(CONFIG_PATH);
     if (config_exists < 0) return 1;
     if (config_exists == 0) {
         nob_log(NOB_INFO, "Generating %s", CONFIG_PATH);
-        Nob_String_Builder content = {0};
         generate_default_config(&content);
         if (!nob_write_entire_file(CONFIG_PATH, content.items, content.count)) return 1;
     } else {
         nob_log(NOB_INFO, "file `%s` already exists", CONFIG_PATH);
     }
+
+    nob_log(NOB_INFO, "Generating build/config_logger.c");
+    content.count = 0;
+    generate_config_logger(&content);
+    if (!nob_write_entire_file("build/config_logger.c", content.items, content.count)) return 1;
 
     Nob_Cmd cmd = {0};
     const char *configured_binary = "build/nob.configured";
